a7c2b592af1146e50181539fc0acc6b5c2ee6757
Nicklas Bregenborg Sivertsen <nicklas_sivertsen@hotmail.com>
2026-09-23 14:13:09 +0200

    made a little js on details.js

--- details.js
+++ details.js
@@ -4,3 +4,24 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
 console.log(id);
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+
+function render () {
+    rootDomDetails.innerHTML = ""
+
+    rootDomDetails.innerHTML = /*HTML*/
+    `
+        <h1>${}</h1>
+    `
+}
+
+function init () {
+fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(function (response) { return response.json() })
+        .then(function (data) {
+            console.log(data)
+            render()
+        })
+}
+init()
