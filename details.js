c7c37fc84a37311acac694feebbdba9b891b24ec
Nicklas Bregenborg Sivertsen <nicklas_sivertsen@hotmail.com>
2026-09-24 10:21:51 +0200

    added html elements into details.js

--- details.js
+++ details.js
@@ -5,23 +5,57 @@ const id = params.get("id");
 
 console.log(id);
 
-fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 
-function render () {
-    rootDomDetails.innerHTML = ""
 
+function render(data) {
+    
+    const pokemonStats = data.stats.map(function (currentStats){
+        const percentNumber = `${currentStats.base_stat}`.padStart(3, "0")
+        return /*HTML*/`
+            <p>${currentStats.stat.name}</p> <div>${percentNumber} <div class="percent_bar"></div></div>
+        `
+    }).join("")
     rootDomDetails.innerHTML = /*HTML*/
-    `
-        <h1>${}</h1>
+        `
+        <img src="assets/Pokeball.svg" alt="Pokeball" class="background_pokeball">
+        <div>
+            <a href="index.html"><i class="fa-solid fa-arrow-left go-back"></i></a>
+            <h1>${data.name}</h1>
+            <p>#${id.padStart(3, "0")}</p>
+        </div>
+        <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
+        <i class="fa-solid fa-greater-than next_pokemon"></i>
+        <h2>About</h2>
+        <div>
+            <p><i class="fa-solid fa-weight-hanging"></i> ${data.weight} <br>
+               <span class="about_information">Weight</span>
+            </p>
+
+            <p><i class="fa-solid fa-ruler-vertical"></i> ${data.height} <br>
+                <span class="about_information">Height</span>
+            </p>
+
+            <p>${data.abilities["0"].ability.name} <br>
+                ${data.abilities["1"].ability.name} <br>
+                <span class="about_information">Moves</span>
+            </p>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum tenetur, ad saepe, deserunt eveniet iure esse qui inventore aut aliquid.</p>
+        <h2>Base Stats</h2>
+        <div>
+            <p>${pokemonStats}</p>
+        </div>
     `
 }
 
-function init () {
-fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(function (response) { return response.json() })
+function init() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(function (response) {
+            return response.json()
+        })
         .then(function (data) {
             console.log(data)
-            render()
+            render(data)
         })
 }
 init()
