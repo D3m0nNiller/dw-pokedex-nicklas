4c6a58714ca1a8457b2655c50c5b503265a81ea6
Nicklas Bregenborg Sivertsen <nicklas_sivertsen@hotmail.com>
2026-09-25 11:06:16 +0200

    styled and finished details without the extra stuff like switching colors and that. (missing types)

--- details.js
+++ details.js
@@ -13,11 +13,18 @@ function render(data) {
 
     titleDom.textContent = `${data.name}`
     
-    const pokemonStats = data.stats.map(function (currentStats){
-        const percentNumber = `${currentStats.base_stat}`.padStart(3, "0")
-        return /*HTML*/`
-            <p>${currentStats.stat.name}</p> <div>${percentNumber} <div class="percent_bar"></div></div>
-        `
+    const pokemonStats = data.stats.map(function (currentStats) {
+    const stat = currentStats.base_stat
+    const percentNumber = `${stat}`.padStart(3, "0")
+    const percentage = (stat / 255) * 100
+
+    return /*HTML*/`
+        <p id="${currentStats.stat.name}">${currentStats.stat.name}</p>
+        <div>${percentNumber}</div>
+        <div class="percent_bar" style="background-color: #E3F5DA; border-radius: 1em; overflow: hidden;">
+            <div class="percent_fill" style=" width: ${percentage}%; background-color: #74CB48; height: 100%;"></div>
+        </div>
+    `
     }).join("")
     rootDomDetails.innerHTML = /*HTML*/
         `
@@ -30,24 +37,33 @@ function render(data) {
         <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
         <i class="fa-solid fa-greater-than next_pokemon"></i>
         <div id="all_information">
-            <h2>About</h2>
-            <p><i class="fa-solid fa-weight-hanging"></i> ${data.weight} <br>
+            <h2 id="small_information">About</h2>
+            <p id="weight">
+                <span id="weight_number">
+                    <i class="fa-solid fa-weight-hanging"></i> ${data.weight} kg 
+                </span><br>
                <span class="about_information">Weight</span>
             </p>
 
-            <p><i class="fa-solid fa-ruler-vertical"></i> ${data.height} <br>
+            <p id="height"> 
+                <span id="height_number">
+                    <i class="fa-solid fa-ruler-vertical"></i> ${data.height} m
+                </span><br>
                 <span class="about_information">Height</span>
             </p>
 
-            <p>${data.abilities["0"].ability.name} <br>
-                ${data.abilities["1"].ability.name} <br>
+            <p id="moves">
+            <span id="moves_named">
+                ${data.abilities["0"].ability.name} <br>
+                ${data.abilities["1"].ability.name} 
+            </span><br>
                 <span class="about_information">Moves</span>
             </p>
         
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum tenetur, ad saepe, deserunt eveniet iure esse qui inventore aut aliquid.</p>
-            <h2>Base Stats</h2>
-            <div>
-                <p>${pokemonStats}</p>
+            <p id="about_pokemon">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum tenetur, ad saepe, deserunt eveniet iure esse qui inventore aut aliquid.</p>
+            <h2 id="intro_stats">Base Stats</h2>
+            <div id="pokemon_stats">
+                ${pokemonStats}
             </div>
         </div>
     `
